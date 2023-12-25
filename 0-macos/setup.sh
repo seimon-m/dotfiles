84c74a09a171b01bf9dd05a463b19582a252128d
Simon <simimueller@hotmail.com>
2023-12-25 12:27:53 +0100

    Update setup.sh

--- 0-macos/setup.sh
+++ 0-macos/setup.sh
@@ -26,7 +26,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 ###############################################################################
 
 # Set computer name (as done via System Preferences → Sharing)
-sudo scutil --set ComputerName "simon"
+sudo scutil --set ComputerName "simon-mpb"
 sudo scutil --set HostName "simon"
 sudo scutil --set LocalHostName "simon"
 sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "simon"
